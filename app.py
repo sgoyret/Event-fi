168690bab097aa38fd1e7d301866e9e016f80cc4
BergeDios <santiago.goyret@gmail.com>
2022-06-02 12:02:19 -0300

    htmel gets styles

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from pickle import TRUE
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,4 +8,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(port=5001)
+    app.run(port=5001, debug=True)
