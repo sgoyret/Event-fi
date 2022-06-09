5a10d45ac49a9ddc7f7f11462336ff16645fe0fd
BergeDios <santiago.goyret@gmail.com>
2022-06-08 21:25:57 -0300

    app.py session kinda works, register for user works

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 from flask import Flask, render_template, session, request, redirect, url_for, session, flash
 from flask_cors import CORS
 from flask_session import Session
-from pymongo import MongoClient
+from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash
 
 
 app = Flask(__name__)
-mongoClient = MongoClient('mongodb+srv://Eventify:superuser@cluster0.cm2bh.mongodb.net/test')
-eventifydb = mongoClient['EVdb']
+
+app.config['MONGO_URI'] = 'mongodb://localhost/events_db'
+mongo = PyMongo(app)
+
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+app.secret_key = 'super secret key'
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "mongodb"
-app.config["SESSION_MONGODB"] = mongoClient
-app.config["SESSION_MONGODB_DB"] = "EVdb"
+app.config["SESSION_MONGODB"] = mongo.db
+app.config["SESSION_MONGODB_DB"] = "events_db"
 app.config["SESSION_MONGODB_COLLECT"] = "sessions"
-Session(app)
-
+ses = Session(app)
 
 @app.route('/index', strict_slashes=False)
 def index():
@@ -27,16 +29,15 @@ def index():
 def register():
     "create user account"
     if request.method == 'POST':
-            name = request.form.get('name')
-            last_name = request.form.get('last_name')
-            password = request.form.get('password')
-            email = request.form.get('email')
+            name = request.json.get('name')
+            last_name = request.json.get('last_name')
+            password = request.json.get('password')
+            email = request.json.get('email')
             if name and last_name and password and email:
                 hashed_password = generate_password_hash(password)
-                collection = eventifydb['users']
-                userdata = {'name': name, 'last_name': last_name, 'email': email, 'password': hashed_password}
-                id = collection.insert_one(userdata)
-                print(id)
+                id = mongo.db.users.insert_one(
+                    {'name': name, 'last_name': last_name, 'email': email, 'password': hashed_password}
+                )
             session["name"] = last_name
             return redirect(url_for('index'))
     return render_template('register.html')
