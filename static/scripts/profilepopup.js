07de1ccc9de1a3b8d4c0f51c97a2858dd1aee335
Diekkan <merentiel.diego@gmail.com>
2022-06-15 16:48:50 -0300

    Jinja, dinamically produce output for user profile

--- static/scripts/profilepopup.js
+++ static/scripts/profilepopup.js
@@ -1,76 +1,38 @@
 window.addEventListener("load", function() {
-    var request = new XMLHttpRequest();
-    request.open('GET', '/api/events')
-    request.send()
-    request.onload = function() {
-        const eventdata = JSON.parse(request.responseText);
-        if (eventdata === null) {
-            document.querySelector('.list').innerHTML =
-            '<li style="text-align: center; margin-top: 30px; display:block;"> Parece que no tienes ningún evento próximamente</li>'
-        }
-        else {
-            var prototype = ''
-            for (let [key, value] of Object.entries(eventdata))
-            {
-                prototype +=             
-                '<li>' +
-                '<div class="listed">' +
-                    '<div class="image">' +
-                        '<div class="img">' +
-                        '</div>' +
-                    '</div>' +
-                    '<div class="info">' +
-                    '<p>'+ value.title  +'</p>' +
-                    '<p>' + value.date + '</p>' +
-                    '<p>' + value.location + '</p>' +
-                    '</div>' +
-                    '<div class="manage">' +
-                        '<div class="manage-button">' +
-                            '<button>Ver</button>' +
-                        '</div>' +
-                        '<div class="manage-button">' +
-                            '<button>Editar</button>' +
-                        '</div>' +
-                        '<div class="manage-button">' +
-                            '<button>Eliminar</button>' +
-                        '</div>' +
-                    '</div>' +
-                '</div>' +
-            '</li>'
-        };
-            }
-            document.querySelector('.list').innerHTML = prototype;
-        console.log(eventdata);
-    };
     this.document.querySelector('.events').addEventListener("click", function() {
-        const selected = document.getElementsByClassName('selected')
+        const selected = document.getElementsByClassName('selected');
+        const active = document.getElementsByClassName('active');
         console.log(selected);
         for (let element of selected) {
             element.classList.remove('selected');
         };
+        document.getElementById('userevents').classList.remove('none');
+        document.getElementById('usergroups').classList.add('none');
+        document.getElementById('usercontacts').classList.add('none');
         this.classList.add('selected');
-        var request = new XMLHttpRequest();
-        request.open('GET', '/api/events')
-        request.send()
-        request.onload = function() {
-            var data = JSON.parse(request.responseText);
-            console.log(data)
-        };
     });
     this.document.querySelector('.groups').addEventListener("click", function() {
-        const selected = document.getElementsByClassName('selected')
+        const selected = document.getElementsByClassName('selected');
+        const active = document.getElementsByClassName('active');
         console.log(selected);
         for (let element of selected) {
             element.classList.remove('selected');
         };
+        document.getElementById('usergroups').classList.remove('none');
+        document.getElementById('userevents').classList.add('none');
+        document.getElementById('usercontacts').classList.add('none');
         this.classList.add('selected');
     });
     this.document.querySelector('.contacts').addEventListener("click", function() {
         const selected = document.getElementsByClassName('selected')
+        const active = document.getElementsByClassName('active')
         console.log(selected);
         for (let element of selected) {
             element.classList.remove('selected');
         };
+        document.getElementById('usercontacts').classList.remove('none');
+        document.getElementById('usergroups').classList.add('none');
+        document.getElementById('userevents').classList.add('none');
         this.classList.add('selected');
     });
 });
